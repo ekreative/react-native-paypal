325098b3454e1d72d2f90c4743034c30e3c06867
Bohdan Rybak <bohdan.rybak@ekreative.com>
2021-12-14 11:56:02 +0200

    feat: catch unexpected errors in paypal-checkout-sdk

--- android/src/main/java/com/reactnativepaypal/Constants.kt
+++ android/src/main/java/com/reactnativepaypal/Constants.kt
@@ -2,5 +2,6 @@ package com.reactnativepaypal
 
 object Constants {
   const val PAYPAL_ERROR_CODE = "PAYPAL_ERROR"
+  const val PAYPAL_INTERNAL_ERROR_CODE = "PAYPAL_INTERNAL_ERROR"
   const val PAYPAL_CANCEL_DEFAULT_CODE = "PAYPAL_CANCELLED"
 }
